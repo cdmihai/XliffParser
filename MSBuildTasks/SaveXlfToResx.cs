c11b929f3c932106ca83d479d1b9d2b7e39123b9
Mihai Codoban <micodoba@microsoft.com>
2016-06-24 17:44:22 -0700

    Add MSBuild tasks project

---
+++ MSBuildTasks/SaveXlfToResx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using xlflib;
+
+namespace xlflib.MSBuildTasks
+{
+    public class SaveXlfToResx : Task
+    {
+        [Required]
+        public string ResxPath { get; set; }
+
+        [Required]
+        public string XlfPath { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                var xlfDocument = new XlfDocument(XlfPath);
+                xlfDocument.SaveAsResX(ResxPath);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.LogError($"Failed to convert xlf file \"{XlfPath}\" to Resx and save it to \"{ResxPath}\"");
+                Log.LogErrorFromException(e);
+                return false;
+            }
+        }
+    }
+}
---
+++ MSBuildTasks/UpdateXlfFromResx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using xlflib;
+
+namespace xlflib.MSBuildTasks
+{
+    public class UpdateXlfFromResx : Task
+    {
+        [Required]
+        public string ResxPath { get; set; }
+
+        [Required]
+        public string XlfPath { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                var xlfDocument = new XlfDocument(XlfPath);
+                Tuple<int, int, int> result = xlfDocument.UpdateFromResX(ResxPath);
+
+                Log.LogMessage(MessageImportance.Low,
+                                "Update results:" +
+                                $"\n\t{result.Item1} resources updated" +
+                                $"\n\t{result.Item2} resources added" +
+                                $"\n\t{result.Item3} resources deleted");
+
+                xlfDocument.Save();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.LogError($"Failed to update xlf file \"{XlfPath}\" from \"{ResxPath}\"");
+                Log.LogErrorFromException(e, true);
+                return false;
+            }
+        }
+    }
+}
